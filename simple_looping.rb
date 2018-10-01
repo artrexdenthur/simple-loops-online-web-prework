ea203348e6010a07b84b48ba4ab2b5c81d9e665f
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 04:16:56 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     puts phrase
-    break if i >= number_of_times
     i += 1
+    break if i >= number_of_times
   end
 end
 
